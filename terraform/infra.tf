2aa4e1ee82c401612268fe105b21109e55988e16
Steve Moreno <smoreno812@live.com>
2017-07-03 12:58:34 -0500

    add vm for demo

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -76,3 +76,40 @@ resource "azurerm_storage_container" "demostoragecontainer" {
     container_access_type = "private"
     depends_on = ["azurerm_storage_account.demostorage"]
 }
+
+# create virtual machine demo01
+resource "azurerm_virtual_machine" "demo01vm" {
+    name = "l_demo01_vm"
+    location = "West US"
+    resource_group_name = "${azurerm_resource_group.coe_infrastructure.name}"
+    network_interface_ids = ["${azurerm_network_interface.demo_nic_01.id}"]
+    vm_size = "Standard_A0"
+
+    storage_image_reference {
+        publisher = "Canonical"
+        offer = "UbuntuServer"
+        sku = "14.04.2-LTS"
+        version = "latest"
+    }
+
+    storage_os_disk {
+        name = "myosdisk"
+        vhd_uri = "${azurerm_storage_account.demostorage.primary_blob_endpoint}${azurerm_storage_container.demostoragecontainer.name}/myosdisk.vhd"
+        caching = "ReadWrite"
+        create_option = "FromImage"
+    }
+
+    os_profile {
+        computer_name = "hostname"
+        admin_username = "steve"
+        admin_password = "Boko812!"
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = false
+    }
+
+    tags {
+        environment = "staging"
+    }
+}
